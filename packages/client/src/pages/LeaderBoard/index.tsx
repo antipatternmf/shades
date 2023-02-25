a1bc59a00a73883721d7d1226b07e147e2b96674
Nikita Tyunev <tyunev.nik89@gmail.com>
2023-02-25 21:46:17 +0300

    add pages components

---
+++ packages/client/src/pages/LeaderBoard/index.tsx
@@ -0,0 +1,16 @@
+import classNames from 'classnames/bind';
+
+import React from 'react';
+import styles from './style.module.pcss';
+
+const cx = classNames.bind(styles);
+
+function LeaderBoard() {
+  return (
+    <div className={cx('container')}>
+      LeaderBoard
+    </div>
+  );
+}
+
+export default LeaderBoard;
