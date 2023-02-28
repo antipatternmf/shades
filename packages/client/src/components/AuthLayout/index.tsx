83a67ae067e79c5d7ba99de750f373197c7b61ca
Nikita Tyunev <tyunev.nik89@gmail.com>
2023-02-28 13:10:25 +0300

    fix

--- packages/client/src/components/AuthLayout/index.tsx
+++ packages/client/src/components/AuthLayout/index.tsx
@@ -1,18 +1,17 @@
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '../../store';
+import { useAppSelector } from '../../store';
 import { PathTo } from '../../constants';
 
 export default function AuthLayout() {
   const isAuth = useAppSelector((state) => state.user.isAuth);
   const navigate = useNavigate();
-  const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (!isAuth) {
       navigate(PathTo.SIGN_IN);
     }
-  }, [isAuth, navigate, dispatch]);
+  }, [isAuth, navigate]);
 
   return (
     <main>
