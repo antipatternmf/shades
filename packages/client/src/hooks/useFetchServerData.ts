db9d2e374b778c932ad595176627bc596671af58
Magomed Aliev <magomed157893@yandex.ru>
2023-03-04 17:53:17 +0300

    update

---
+++ packages/client/src/hooks/useFetchServerData.ts
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+
+export const useFetchServerData = () => {
+  useEffect(() => {
+    const fetchServerData = async () => {
+      const url = `http://localhost:${SERVER_PORT}`;
+      const response = await fetch(url);
+      const data = await response.json();
+
+      // eslint-disable-next-line no-console
+      console.log(data);
+    };
+    fetchServerData();
+  }, []);
+};
