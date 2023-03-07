d61178fee8b146be59b4bbc507d929ef5e444b6d
Magomed Aliev <magomed157893@yandex.ru>
2023-03-07 19:32:42 +0300

    Init 23AP-17

--- packages/client/src/pages/Forum/index.tsx
+++ packages/client/src/pages/Forum/index.tsx
@@ -1,14 +1,34 @@
 import classNames from 'classnames/bind';
-
-import React from 'react';
 import styles from './style.module.pcss';
 
 const cx = classNames.bind(styles);
 
 function Forum() {
   return (
-    <div className={cx('container')}>
-      Forum
+    <div className={cx('wrapper')}>
+      <div className={cx(styles.forum, 'container')}>
+        <table>
+          <thead>
+            <tr>
+              <th>Форумы</th>
+              <th>Темы</th>
+              <th>Ответы</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Вопросы/Предложения</td>
+              <td>11</td>
+              <td>54</td>
+            </tr>
+            <tr>
+              <td>Флудилка</td>
+              <td>222</td>
+              <td>657</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
