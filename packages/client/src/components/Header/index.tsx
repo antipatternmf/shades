6894fd29d4e2cf6d45a479dbb51979fa6b202325
AlekseyRomanov <aromanov@domprog.com>
2023-03-22 14:20:04 +0300

    Сверстать страницу лидерборда

--- packages/client/src/components/Header/index.tsx
+++ packages/client/src/components/Header/index.tsx
@@ -4,30 +4,30 @@ import styles from './styles.module.pcss';
 
 type HeaderProps = {
   title?: string;
-  isShowBackButton?: boolean
+  isShowBackButton?: boolean;
 };
 
 const cx = classNames.bind(styles);
 
 export default function Header({
-                                 title = '',
-                                 isShowBackButton = true,
-                               }: HeaderProps) {
+  title = '',
+  isShowBackButton = true,
+}: HeaderProps) {
   const navigate = useNavigate();
 
   return (
     <header className={cx(styles.header)}>
       <div>
-        {
-          isShowBackButton ? (
-            <button
-              className={cx(styles.headerBackButton, 'shadow')}
-              onClick={() => navigate(-1)}
-            >
-              &#8592;
-            </button>
-) : ''
-        }
+        {isShowBackButton ? (
+          <button
+            className={cx(styles.headerBackButton, 'shadow')}
+            onClick={() => navigate(-1)}
+          >
+            &#8592;
+          </button>
+        ) : (
+          ''
+        )}
       </div>
       <h1 className={cx(styles.headerTitle)}>{title}</h1>
       <div />
---
+++ packages/client/src/pages/LeaderBoard/LeaderList/index.tsx
@@ -0,0 +1,17 @@
+import classNames from 'classnames/bind';
+import { ReactNode } from 'react';
+import styles from './styles.module.pcss';
+
+type LeaderListProps = {
+  children?: ReactNode;
+};
+
+const cx = classNames.bind(styles);
+
+export default function LeaderList({ children }: LeaderListProps) {
+  return (
+    <div className={cx(styles.leaderList)}>
+      {children}
+    </div>
+  );
+}
